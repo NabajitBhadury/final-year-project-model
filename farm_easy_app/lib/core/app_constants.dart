411257cb4a1ec01a643d85d1ab4f8e402a1f6afb
NabajitBhadury <nabajitbhadury@gmail.com>
2026-06-16 01:41:59 +0530

    Initial commit

--- farm_easy_app/lib/core/app_constants.dart
+++ farm_easy_app/lib/core/app_constants.dart
@@ -5,7 +5,7 @@ class AppConstants {
 
   static const String apiBaseUrl = String.fromEnvironment(
     'FARMEASY_API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:8000',
+    defaultValue: 'https://final-year-project-model-production.up.railway.app',
   );
 
   static const String healthPath = '/health';
